6bd7618f364128590440fe12fc80d8b9c92e6322
Victor <lessricc@gmail.com>
2024-05-17 19:01:20 +0200

    Cambios

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,14 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
 
 const array = [1, 2, 3];
 
-const randomArray = array => {
+const randomArray = (array) => {
   const numRamdom = Math.floor(Math.random() * array.length);
   console.log(array[numRamdom]);
 };
 randomArray(array);
-
-<<<<<<< HEAD
-const threeNumber = [1, 5, 7];
+const threeNumbers = [3, 6, 9];
 
 const higherAndLess = (numbers) => {
   if (
@@ -30,32 +28,32 @@ const threeNumberMedia = (arrayAddition) => {
   ) {
     console.log(`El número mayor es ${arrayAddition}`);
   }
-=======
-const threeNumbers = [3, 6, 9];
->>>>>>> 7326dfe71a87ed6a4736d7e0e5bcd3e97a5c030c
 
-const threeNumberMedia = array => {
-  const addition = array[0] + array[1] + array[2];
-  console.log(`La suma de todos los numeros es ${addition}`);
-  const media = (array[0] + array[1] + array[2]) / array.length;
-  console.log(`La media de todos los numeros es ${media}`);
+  const threeNumberMedia = (array) => {
+    const addition = array[0] + array[1] + array[2];
+    console.log(`La suma de todos los numeros es ${addition}`);
+    const media = (array[0] + array[1] + array[2]) / array.length;
+    console.log(`La media de todos los numeros es ${media}`);
 
-  const max = Math.max(array[0], array[1], array[2]);
-  const min = Math.min(array[0], array[1], array[2]);
-  console.log(`El mayor es ${max} y el menor es ${min}`);
+    const max = Math.max(array[0], array[1], array[2]);
+    const min = Math.min(array[0], array[1], array[2]);
+    console.log(`El mayor es ${max} y el menor es ${min}`);
+  };
+  threeNumberMedia(threeNumbers);
 };
-
 threeNumberMedia(threeNumbers);
 
-const fiveNumbers = array => {
+const fiveNumbers = (array) => {
   const numRamdom = Math.floor(Math.random() * 11);
   if (array.includes(numRamdom)) {
-    console.log(`El array contiene ${numRamdom} y su posición ${array.indexOf(numRamdom)}`);
+    console.log(
+      `El array contiene ${numRamdom} y su posición ${array.indexOf(numRamdom)}`
+    );
   } else console.log(`El número no está`);
 };
 fiveNumbers([1, 3, 6, 7, 8]);
 
-const ghostNumbers = array => {
+const ghostNumbers = (array) => {
   array.push(Math.floor(Math.random() * 101));
   array.push(Math.floor(Math.random() * 101));
   array.push(Math.floor(Math.random() * 101));
@@ -63,7 +61,7 @@ const ghostNumbers = array => {
 };
 ghostNumbers([]);
 
-const evenAndOdd = array => {
+const evenAndOdd = (array) => {
   const arrayEven = [];
   const arrayOdd = [];
   const numbRandom = Math.floor(Math.random() * 10 + 1);
